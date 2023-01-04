3ef3a3f92031ca180e48210b0c066e546f66baea
kofi amoussou <karsoft92@gmail.com>
2023-01-04 15:58:26 +0000

    Consolidate migration

--- database/migrations/settings/2022_11_22_100000_add_server_side_url_to_elevar_settings.php
+++ database/migrations/settings/2022_11_22_100000_add_server_side_url_to_elevar_settings.php
@@ -7,10 +7,13 @@ class AddServerSideUrlToElevarSettings extends SettingsMigration
     public function up(): void
     {
         $this->migrator->add('elevar.server_side_url', '');
+        $this->migrator->add('elevar.data_layer_listener_enabled', false);
+
     }
 
     public function down()
     {
         $this->migrator->delete('elevar.server_side_url');
+         $this->migrator->delete('elevar.data_layer_listener_enabled');
     }
 }
