b79f7af88b8b177fe6b71eb0f05f8cccb3e51370
Kristoffer Tonning <github@tonning.dev>
2023-01-05 10:14:33 +0100

    Fix styling

--- database/migrations/settings/2022_11_22_100000_add_server_side_url_to_elevar_settings.php
+++ database/migrations/settings/2022_11_22_100000_add_server_side_url_to_elevar_settings.php
@@ -8,12 +8,11 @@ class AddServerSideUrlToElevarSettings extends SettingsMigration
     {
         $this->migrator->add('elevar.server_side_url', '');
         $this->migrator->add('elevar.data_layer_listener_enabled', false);
-
     }
 
     public function down()
     {
         $this->migrator->delete('elevar.server_side_url');
-         $this->migrator->delete('elevar.data_layer_listener_enabled');
+        $this->migrator->delete('elevar.data_layer_listener_enabled');
     }
 }
--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,53 +1,50 @@
-
 @php($settings = settings(Astrogoat\Elevar\Settings\ElevarSettings::class))
 
 @if($settings->isEnabled() && $settings->data_layer_listener_enabled)
-        <!-- [ELEVAR] Start -->
-        <script id="elevar-dl-listener-config" type="application/json">
-            {
+    <!-- [ELEVAR] Start -->
+    <script id="elevar-dl-listener-config" type="application/json">
+        {
             "data_layer_listener_script": "https://shopify-gtm-suite.getelevar.com/shops/{{ $settings->uuid }}/events.js",
-             "ss_url": {!! blank($settings->server_side_url) ? 'null' : '"' . $settings->server_side_url . '"' !!}
-            }
-        </script>
-        <script>
-            (function () {
-                const configElement = document.getElementById("elevar-dl-listener-config");
+            "ss_url": {!! blank($settings->server_side_url) ? 'null' : '"' . $settings->server_side_url . '"' !!}
+        }
+    </script>
+    <script>
+        (function () {
+            const configElement = document.getElementById("elevar-dl-listener-config");
 
-                if (!configElement) {
-                    console.error("Elevar Data Layer Listener: Config element not found");
+            if (!configElement) {
+                console.error("Elevar Data Layer Listener: Config element not found");
+                return;
+            }
+
+            const config = JSON.parse(configElement.textContent);
+
+            const script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = config.data_layer_listener_script;
+
+            script.onerror = function () {
+                console.error("Elevar Data Layer Listener: JS script failed to load");
+            };
+            script.onload = function () {
+                if (!window.ElevarGtmSuiteListener) {
+                    console.error(
+                        "Elevar Data Layer Listener: `ElevarGtmSuiteListener` is not defined"
+                    );
                     return;
                 }
 
-                const config = JSON.parse(configElement.textContent);
+                window.ElevarGtmSuiteListener.handlers.listen({ ssUrl: config.ss_url });
+            };
 
-                const script = document.createElement("script");
-                script.type = "text/javascript";
-                script.src = config.data_layer_listener_script;
+            const headScripts = document.head.getElementsByTagName("script");
 
-                script.onerror = function () {
-                    console.error("Elevar Data Layer Listener: JS script failed to load");
-                };
-                script.onload = function () {
-                    if (!window.ElevarGtmSuiteListener) {
-                        console.error(
-                            "Elevar Data Layer Listener: `ElevarGtmSuiteListener` is not defined"
-                        );
-                        return;
-                    }
-
-                    window.ElevarGtmSuiteListener.handlers.listen({ ssUrl: config.ss_url });
-                };
-
-                const headScripts = document.head.getElementsByTagName("script");
-
-                if (headScripts[0]) {
-                    document.head.insertBefore(script, headScripts[0]);
-                } else {
-                    document.head.appendChild(script);
-                }
-            })();
-        </script>
-        <!-- [ELEVAR] End -->
-    @endif
-
-
+            if (headScripts[0]) {
+                document.head.insertBefore(script, headScripts[0]);
+            } else {
+                document.head.appendChild(script);
+            }
+        })();
+    </script>
+    <!-- [ELEVAR] End -->
+@endif
